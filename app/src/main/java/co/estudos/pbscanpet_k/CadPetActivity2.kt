c208b82ea8a3350f239b7364de276b8a08815a8c
Cesar Nullo <paulo.camuri@outlook.com>
2020-10-16 15:44:30 -0300

    Inside of class CadPetActivity2 -->  btnDone.setOnClickListener, was created and implemented --> Run app checked ok!

--- app/src/main/java/co/estudos/pbscanpet_k/CadPetActivity2.kt
+++ app/src/main/java/co/estudos/pbscanpet_k/CadPetActivity2.kt
@@ -2,6 +2,8 @@ package co.estudos.pbscanpet_k
 
 import android.content.Intent
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_cad_pet2.*
 
@@ -9,6 +11,18 @@ class CadPetActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cad_pet2)
+
+
+
+
+
+
+        /*when the button btnDone is pressed, it will calls the createAnimal() fun
+         and before createAnimal() ends, it will return to the MenuActivity
+         by the intent.
+         It's important to know that by exception in case of the update fails, a message
+         toast must be displayed for the user of the system.
+        */
 
         btnDone.setOnClickListener{
             val intent = Intent(this, MenuActivity::class.java)
@@ -20,4 +34,19 @@ class CadPetActivity2 : AppCompatActivity() {
 
 
     }
+
+
+
+    /*Here is fun selectPhoto()
+    Basically this fun is responsible for
+    get images from gallery. Take look above.
+     */
+    fun selectPhoto(){
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, 0)
+    }
+
+
+
 }
--- app/src/main/java/co/estudos/pbscanpet_k/MapaActivity.kt
+++ app/src/main/java/co/estudos/pbscanpet_k/MapaActivity.kt
@@ -1,4 +1,8 @@
 package co.estudos.pbscanpet_k
 
 class MapaActivity {
+
+    fun scan(){
+        // Função de escaneamento de barcode
+    }
 }
