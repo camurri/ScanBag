7a262a767db9f7b4acd78236993fcabb9bc788a6
Cesar Nullo <paulo.camuri@outlook.com>
2020-10-31 11:29:55 -0300

    added -> implementation 'com.google.android.gms:play-services-auth:18.1.0' --> Run app checked ok!

--- app/src/main/java/co/estudos/pbscanpet_k/CadOwnerActivity.kt
+++ app/src/main/java/co/estudos/pbscanpet_k/CadOwnerActivity.kt
@@ -1,8 +1,9 @@
 package co.estudos.pbscanpet_k
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.bundleOf
 import kotlinx.android.synthetic.main.activity_cad_owner.*
 
 class CadOwnerActivity : AppCompatActivity() {
@@ -14,6 +15,13 @@ class CadOwnerActivity : AppCompatActivity() {
             val intent = Intent(this, CadOwnerActivity2::class.java)
             startActivity(intent)
         }
+
+
+        //Crate a new Bundle to pass values to CadOwnerActivity2
+
+        val bundle = bundleOf("name" to editTxtName)
+       // val navigate: Any = view.findNavController().navigate(R.id.txtViewYourName, bundle)
+
 
     }
 }
--- app/src/main/java/co/estudos/pbscanpet_k/CadOwnerActivity2.kt
+++ app/src/main/java/co/estudos/pbscanpet_k/CadOwnerActivity2.kt
@@ -3,6 +3,7 @@ package co.estudos.pbscanpet_k
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_cad_owner.*
 import kotlinx.android.synthetic.main.activity_cad_owner2.*
 
 class CadOwnerActivity2 : AppCompatActivity() {
@@ -14,6 +15,7 @@ class CadOwnerActivity2 : AppCompatActivity() {
             val intent = Intent(this, MenuActivity::class.java)
             startActivity(intent)
         }
+
 
 
     }
